bd39d63985bfb1e3fb512f9142cc604ff28e0fd8
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2023-01-24 10:51:16 +0000

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

--- MyFirstAndroid/Assets/Scripts/TouchManagerScript.cs
+++ MyFirstAndroid/Assets/Scripts/TouchManagerScript.cs
@@ -18,13 +18,42 @@ public class TouchManagerScript : MonoBehaviour
             Touch[] myTouches = Input.touches;
             Touch myFirstTouch = myTouches[0];
             print(myFirstTouch.position);
+            switch (myFirstTouch.phase)
+            {
+                case TouchPhase.Began:
+
+                    break;
+
+                case TouchPhase.Stationary:
+
+                    break;
+
+                case TouchPhase.Moved:
+
+                    break;
+
+                case TouchPhase.Ended:
+
+                    break;
+            
+            }
+
+
+
+
+
+
             Ray myRay = Camera.main.ScreenPointToRay(myFirstTouch.position);
             Debug.DrawRay(myRay.origin, 15 * myRay.direction);
             RaycastHit hitInfo;
             if (Physics.Raycast(myRay,out hitInfo))
             {
-                SpaceShipScript touchedSpaceShip = hitInfo.transform.GetComponent<SpaceShipScript>();
-                touchedSpaceShip.changeColor(Color.green);
+                ITouchable touchedObject = hitInfo.transform.GetComponent<ITouchable>();
+                if (touchedObject != null)
+                    touchedObject.OnTap();
+
+                if (touchedObject is SpaceShipScript)
+                    (touchedObject as SpaceShipScript).changeColor(Color.green);
             }
 
 
